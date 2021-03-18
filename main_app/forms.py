f785159b8f08dc258b79aa75ca372d3778b6f9a5
tbutler4 <terrencebutler@Mac-mini.local>
2021-03-17 17:24:40 -0700

    fix(styling): finished styling for single review page and grid on home page

--- main_app/forms.py
+++ main_app/forms.py
@@ -16,6 +16,7 @@ class EditUserForm(forms.ModelForm):
         fields = ['username', 'first_name', 'last_name']
 
 class CommentForm(forms.ModelForm):
+    comment_text = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Enter Comment'}))
     class Meta:
         model = Comment
         fields = ['comment_text']
